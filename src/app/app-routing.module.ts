aee37671be3103a9f8cbe885f77da07ca74e1649
Javier Villaseñor García <javiervg0300@gmail.com>
2024-11-06 13:43:13 +0100

    06-11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PoliticaPrivacidadComponent } from './politica-privacidad/politica-privacidad.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'sobre-nosotros', component: SobreNosotrosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'politica-privacidad', component: PoliticaPrivacidadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PoliticaPrivacidadComponent } from './politica-privacidad/politica-privacidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SobreNosotrosComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    PoliticaPrivacidadComponent
   ],
   imports: [
     BrowserModule,
