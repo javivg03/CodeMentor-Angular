b6e14c58d2cade8f238139b44ca1628c2c23f9e2
Javier Villaseñor García <javiervg0300@gmail.com>
2024-11-19 11:56:04 +0100

    añadido reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule, 
     NgbModule
   ],
   providers: [],
--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.sass']
 })
 export class ContactoComponent {
+  // Define el formulario
+  contactoForm: FormGroup;
 
+  constructor() {
+    this.contactoForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+  // Método para manejar el envío
+  onSubmit() {
+    if (this.contactoForm.valid) {
+      console.log('Formulario enviado:', this.contactoForm.value);
+      alert('¡Formulario enviado correctamente!');
+      this.contactoForm.reset(); // Resetea el formulario después del envío
+    } else {
+      alert('Por favor, completa correctamente todos los campos.');
+    }
+  }
 }
